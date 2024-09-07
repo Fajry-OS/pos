c752f5a459b92d4d27f19dc3e5d23864be824a22
Fajry-OS <fajriseptiano@gmail.com>
2024-09-07 08:38:22 +0700

    penambahan middleware ver ppkd

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,14 @@ Route::get('/', [\App\Http\Controllers\LoginController::class, 'index']);
 Route::get('login', [\App\Http\Controllers\LoginController::class, 'index'])->name('login');
 Route::get('logout', [\App\Http\Controllers\LoginController::class, 'logout'])->name('logout');
 
+//Cara logout nggak pake controller
+
+// Route::get('keluar', function(){
+//     Auth::logout();
+//     return redirect()->to('login');
+// })->name('keluar');
+
+
 Route::resource('user', \App\Http\Controllers\UserController::class);
 Route::resource('category', \App\Http\Controllers\CategoryController::class);
 Route::post('action-login', [\App\Http\Controllers\LoginController::class, 'actionLogin'])->name('action-login');
@@ -19,10 +27,13 @@ Route::post('action-login', [\App\Http\Controllers\LoginController::class, 'acti
 Route::middleware(['auth'])->group(function () {
     Route::resource('dashboard', \App\Http\Controllers\DashboardController::class);
     Route::resource('product', \App\Http\Controllers\ProductController::class);
-    Route::resource('penjualan', \App\Http\Controllers\TransactionController::class);
 
 
     Route::get('get-products/{category_id}', [\App\Http\Controllers\TransactionController::class, 'getProducts']);
     Route::get('get-product/{product_id}', [\App\Http\Controllers\TransactionController::class, 'getProduct']);
     Route::get('print/{id}', [\App\Http\Controllers\TransactionController::class, 'print']);
+});
+
+Route::middleware(['checkLevel:3'])->group(function () {
+    Route::resource('penjualan', \App\Http\Controllers\TransactionController::class);
 });
